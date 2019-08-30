bca2200aaf2c2116f0d237c864dbf91101741499
wmkouw <wmkouw@gmail.com>
2019-08-30 22:44:38 +0200

    Started playing with simulating signal

---
+++ initial-experiment/initial_model.jl
@@ -0,0 +1,21 @@
+# Initial model in Julia
+
+""" Parameters """
+
+# Inertia of arm
+M1  = 95.1089;
+
+# Viscous friction
+Fv1 = 203.5034;
+
+# Coulomb friction
+Fc1 = 20.3935;
+
+# Offset
+OF1 = -3.1648;
+
+# Direct dynamic model (DDM)
+
+
+# Inverse dynamic model (IDM)
+τ_idm =
